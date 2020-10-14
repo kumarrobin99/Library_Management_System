caa9a55a7fb36aec25881873cc1ec16a71ee97fe
Robin Kumar <68186209+kumarrobin99@users.noreply.github.com>
2020-10-14 18:22:09 +0530

    Add files via upload

---
+++ src/librarymanagementsystem/LibraryManagementSystem.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package librarymanagementsystem; 
+
+import java.sql.*;
+
+/**
+ *
+ * @author kumar
+ */
+public class LibraryManagementSystem {
+
+ 
+    public static void main(String[] args) {
+        MainWindow mw=new MainWindow();
+        mw.setVisible(true);
+        /*try{   
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_management","root","");  
+            }
+        catch(Exception e)
+            { System.out.println(e);} */
+        // TODO code application logic here
+    }
+}
